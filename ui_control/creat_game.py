d7f9edeb6b176d542f2aec9703503c1d678e6e27
yang <2516840928@qq.com>
2026-05-17 11:01:53 +0800

    Refactor start_step to use single VideoStream for all visual recognition - Template matching: capture_window + cwg → stream.wait_for_template - OCR captures: stream.read() + cv2.imwrite to disk - Single stream reused for user_avatar/user_center calls - Removed capture_window and cwg imports from creat_game.py

--- ui_control/creat_game.py
+++ ui_control/creat_game.py
@@ -1,62 +1,64 @@
 import logging
 from time import sleep
 
-from anasis.utils.compile import compare_with_template_grep as cwg
+import cv2
+
 from anasis.utils.excel_analysis import count_info, part_info, remark_info
-from anasis.utils.photo_utils import photo_path
+from anasis.utils.photo_utils import photo_path, save_photo_path
 from anasis.utils.pp_ocr import choose_part, choose_count, choose_user_one_part
 from game_actions import register
 from game_actions.gouxie import gou_xie
 from game_actions.return_game_login import user_avatar, user_center
 from ui_control.window_control.mouse_action import mouse_click
 from ui_control.window_control.video_stream import VideoStream
-from ui_control.window_control.win import windows_get, capture_window
+from ui_control.window_control.win import windows_get
 
 
 def start_step(count_name, parts, rect):
-    # 截图窗口内容
     sleep(2)
-    capture_window(rect)
-    match_enter = choose_count(count_name, rect)
-    if match_enter:
-        # 进入游戏后重新截屏
-        sleep(5)
-        screenshot = capture_window(rect)
-        exchange = cwg(screenshot, photo_path("exchange.png"))
-        mouse_click(exchange, rect)
-        sleep(1)
-        capture_window(rect)
-        # 选择分区
-        parts, remove_part = choose_part(parts, rect)
-        sleep(3)
-        screenshot = capture_window(rect)
-        # 分区进去游戏
-        exchange = cwg(screenshot, photo_path("enter_game2.png"))
-        logging.info("点击进入游戏")
-        mouse_click(exchange, rect)
-        # 判断是否多分区登录
-        screenshot = capture_window(rect)
-        more_parts = cwg(screenshot, photo_path("more_part.png"))
-        if more_parts is not None:
-            choose_user_one_part(remove_part,exchange,rect)
-        if exchange is not None:
-            sleep(10)
-            # ToDO 增加广告关闭
+    stream = VideoStream(rect)
+    stream.start()
+    try:
+        # OCR: 保存帧供 choose_count 识别
+        frame = stream.read()
+        if frame is not None:
+            cv2.imwrite(save_photo_path("yys.png"), frame)
+        match_enter = choose_count(count_name, rect)
+        if match_enter:
+            sleep(5)
+            match_pos = stream.wait_for_template(photo_path("exchange.png"))
+            if match_pos is not None:
+                mouse_click(match_pos, rect)
+            sleep(1)
+            # OCR: 保存帧供 choose_part 识别
+            frame = stream.read()
+            if frame is not None:
+                cv2.imwrite(save_photo_path("yys.png"), frame)
+            parts, remove_part = choose_part(parts, rect)
+            sleep(3)
+            enter_match = stream.wait_for_template(photo_path("enter_game2.png"))
+            logging.info("点击进入游戏")
+            if enter_match is not None:
+                mouse_click(enter_match, rect)
+            # 判断是否多分区登录
+            more_parts = stream.wait_for_template(photo_path("more_part.png"))
+            if more_parts is not None:
+                choose_user_one_part(remove_part, enter_match, rect)
+            if enter_match is not None:
+                sleep(10)
+                # ToDO 增加广告关闭
 
-            # TODO 勾协与神秘商人可接入
-            gou_xie(rect)
-            sleep(2)
-            stream = VideoStream(rect)
-            stream.start()
-            try:
+                # TODO 勾协与神秘商人可接入
+                gou_xie(rect)
+                sleep(2)
                 user_avatar(stream, rect)
                 sleep(3)
                 user_center(stream, rect)
-            finally:
-                stream.stop()
-        return parts, remove_part
-    else:
-        logging.info("未找到进入游戏按钮")
+            return parts, remove_part
+        else:
+            logging.info("未找到进入游戏按钮")
+    finally:
+        stream.stop()
 
 @register("login")
 def login():
